fb369303827ad6e22ba4425d8f52c36b55d108ff
sw93 <sw93.ok@gmail.com>
2025-05-15 21:52:31 +0300

    all is working, but there is a lot of errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
